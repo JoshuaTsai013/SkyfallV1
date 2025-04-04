cab2ad9c0e48e01df65de242a0ee1e016881da9f
JoshuaTsai013 <joshuatsai1213@gmail.com>
2025-04-04 19:50:32 +0800

    boss fuck up

--- Assets/Scripts/Boss/BossRightGun.cs
+++ Assets/Scripts/Boss/BossRightGun.cs
@@ -6,26 +6,26 @@ public class BossRightGun : MonoBehaviour
     private Animator _animator;
     [SerializeField]
     private float _GunAngle = 0f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
+    [SerializeField]
+    private Transform _gunTransform;
     // Update is called once per frame
     void Update()
     {
         GunAngle();
+        // if (Time.frameCount % 120 == 0)
+        // {
+        //     _animator.SetTrigger("RightGunShot");
+        // }
     }
 
     private void GunAngle()
     {
-        Vector3 direction = PlayerManager.instance.player.transform.position - transform.position;
+        Vector3 direction = PlayerManager.instance.player.transform.position - _gunTransform.position;
+        Debug.DrawLine(_gunTransform.position, PlayerManager.instance.player.transform.position, Color.red);
         float targetAngle = Quaternion.LookRotation(direction).eulerAngles.x;
-        targetAngle = Mathf.Clamp(targetAngle, 0, 90);
-        // _GunAngle = targetAngle;
-        _GunAngle = Mathf.LerpAngle(_GunAngle, targetAngle, Time.deltaTime * 2f);
+        // targetAngle = Mathf.Clamp(targetAngle, 0, 90);
+        _GunAngle = targetAngle;
+        // _GunAngle = Mathf.LerpAngle(_GunAngle, targetAngle, Time.deltaTime * 2f);
         
 
         _animator.SetFloat("GunAngle", _GunAngle);
