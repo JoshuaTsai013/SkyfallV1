2341050b717862d73b12815d03fdd412763f1919
JoshuaTsai013 <joshuatsai1213@gmail.com>
2025-04-10 01:34:38 +0800

    Training Room v1

--- Assets/Scripts/Training/TrainingManager.cs
+++ Assets/Scripts/Training/TrainingManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TrainingManager : MonoBehaviour
@@ -7,13 +8,17 @@ public class TrainingManager : MonoBehaviour
     public Image Jump;
     public Image Dash;
     public Image Shoot;
-    public Image Exit;
+    public Image Melee;
+    public Image Overheat;
+   
 
     public TrainingTrigger MoveCollider;
     public TrainingTrigger JumpCollider;
     public TrainingTrigger NextCollider;
     public TrainingTrigger DashCollider;
     public TrainingTrigger ShootCollider;
+    public TrainingTrigger MeleeCollider;
+    public TrainingTrigger OverheatCollider;
     public TrainingTrigger ExitCollider;
 
     public TrainingDoorOpen door;
@@ -22,49 +27,17 @@ public class TrainingManager : MonoBehaviour
         Jump.gameObject.SetActive(false);
         Dash.gameObject.SetActive(false);
         Shoot.gameObject.SetActive(false);
-        Exit.gameObject.SetActive(false);
-        // PlayerManager.instance.player.transform.SetPositionAndRotation(gameObject.transform.position, gameObject.transform.rotation);
+        Melee.gameObject.SetActive(false);
+        Overheat.gameObject.SetActive(false);
     }
     void Update()
     {
-        if (MoveCollider.stay)
-        {
-            ShowMove();
-        }
-        else
-        {
-            HideMove();
-        }
-
-        if (JumpCollider.stay)
-        {
-            HideMove();
-            ShowJump();
-        }
-        else
-        {
-            HideJump();
-        }
-
-        if (DashCollider.stay)
-        {
-            HideJump();
-            ShowDash();
-        }
-        else
-        {
-            HideDash();
-        }
-
-        if (ShootCollider.stay)
-        {
-            HideDash();
-            ShowShoot();
-        }
-        else
-        {
-            HideShoot();
-        }
+        HandleTrainingStep(MoveCollider, Move);
+        HandleTrainingStep(JumpCollider, Jump);
+        HandleTrainingStep(DashCollider, Dash);
+        HandleTrainingStep(ShootCollider, Shoot);
+        HandleTrainingStep(MeleeCollider, Melee);
+        HandleTrainingStep(OverheatCollider, Overheat);
 
         if (NextCollider.stay)
         {
@@ -75,48 +48,14 @@ public class TrainingManager : MonoBehaviour
 
         if (ExitCollider.stay)
         {
-            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
             gameObject.SetActive(false);
         }
     }
-    private void ShowMove()
-    {
-        Move.gameObject.SetActive(true);
-    }
 
-    private void ShowJump()
+    private void HandleTrainingStep(TrainingTrigger trigger, Image uiElement)
     {
-        Jump.gameObject.SetActive(true);
-    }
-
-    private void ShowDash()
-    {
-        Dash.gameObject.SetActive(true);
-    }
-
-    private void ShowShoot()
-    {
-        Shoot.gameObject.SetActive(true);
-    }
-
-    private void HideMove()
-    {
-        Move.gameObject.SetActive(false);
-    }
-
-    private void HideJump()
-    {
-        Jump.gameObject.SetActive(false);
-    }
-
-    private void HideDash()
-    {
-        Dash.gameObject.SetActive(false);
-    }
-
-    private void HideShoot()
-    {
-        Shoot.gameObject.SetActive(false);
+        uiElement.gameObject.SetActive(trigger.stay);
     }
 }
 
